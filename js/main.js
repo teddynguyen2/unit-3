c2f8503eacbed2a1bde85c10b94f9884f375bd6e
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-04-05 23:27:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+(function (){
+
+	var attrArray = ["Area (km²)", "Median income (€)", "GDP in billions (€)", "Percentage of GDP (%)", "GDP per capita (€)", "Human Development Index (HDI)"]
+	var expressed = attrArray[0];
+	
 //begin script when window loads
 window.onload = setMap();
-
+	
+//set up choropleth map
+function setMap(){
+	
 //map frame dimensions
-    var width = 2400,
-        height = 1900;
+    var width = window.innerWidth * 0.5,
+        height = 460;
 
     //create new svg container for the map
     var map = d3.select("body")
@@ -23,8 +31,6 @@ window.onload = setMap();
 	var path = d3.geoPath()
         .projection(projection);
 
-//set up choropleth map
-function setMap(){
     //use Promise.all to parallelize asynchronous data loading
     var promises = [d3.csv("data/Spain_data.csv"),
                     d3.json("data/Europe.topojson"),
@@ -36,15 +42,30 @@ function callback(data){
     csvData = data[0];
     europe = data[1];
     spain = data[2];
-    console.log(csvData);
-            console.log(europe);
-            console.log(spain);  
-
+	
+//place graticule on the map
+        setGraticule(map, path);
+	
 //translate europe TopoJSON
         var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
             spainRegions = topojson.feature(spain, spain.objects.SpainRegions).features;
 
+//add Europe countries to map
+        var countries = map.append("path")
+            .datum(europeCountries)
+            .attr("class", "countries")
+            .attr("d", path);
+	
+	spainRegions = joinData(spainRegions, csvData);
+        var colorScale = makeColorScale(csvData);
+        setEnumerationUnits(spainRegions, map, path, colorScale);
+        //add coordinated visualization to the map
+        setChart(csvData, colorScale);
+	};
+};
+
 //create graticule generator
+function setGraticule(map, path){
         var graticule = d3.geoGraticule()
             .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
@@ -61,14 +82,70 @@ function callback(data){
             .append("path") //append each element to the svg as a path element
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
+}
 
-//add Europe countries to map
-        var countries = map.append("path")
-            .datum(europeCountries)
-            .attr("class", "countries")
-            .attr("d", path);
+function joinData(spainRegions, csvData){
+//loop through csv to assign each set of csv attribute values to geojson region
+    for (var i=0; i<csvData.length; i++){
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.SpainRegions; //the CSV primary key
+	 
+//loop through geojson regions to find correct region
+        for (var a=0; a<spainRegions.length; a++){
 
-        //add Spain autonomous communities to map
+            var geojsonProps = spainRegions[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.id_code; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+    return spainRegions;
+}
+	
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#ffffcc",
+        "#c2e699",
+        "#78c679",
+        "#31a354",
+        "#006837"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleThreshold()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+
+    //cluster data using ckmeans clustering algorithm to create natural breaks
+    var clusters = ss.ckmeans(domainArray, 5);
+    //reset domain array to cluster minimums
+    domainArray = clusters.map(function(d){
+        return d3.min(d);
+    });
+    //remove first value from domain array to create class breakpoints
+    domainArray.shift();
+
+    //assign array of last 4 cluster minimums as domain
+    colorScale.domain(domainArray);
+    return colorScale;
+};	
+
+function setEnumerationUnits(spainRegions, map, path, colorScale){
+        //add Spain regions to map
         var regions = map.selectAll(".regions")
             .data(spainRegions)
             .enter()
@@ -76,8 +153,119 @@ function callback(data){
             .attr("class", function(d){
                 return "regions " + d.properties.id_code;
             })
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", function(d){
+            return colorScale(d.properties[expressed]);
+            if(value) {
+                return colorScale(d.properties[expressed]);
+            } else {
+                return "#ccc";
+            }
+        });
+     }
 
-	};
-};
+//function to create coordinated bar chart
+function setChart(csvData, colorScale){
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
+
+    //create a rectangle for chart background fill
+    var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+
+    //create a scale to size bars proportionally to frame and for axis
+    var yScale = d3.scaleLinear()
+        .range([463, 0])
+        .domain([0, 100]);
+
+    //set bars for each province
+    var bars = chart.selectAll(".bar")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return b[expressed]-a[expressed]
+        })
+        .attr("class", function(d){
+            return "bars " + d.id_code;
+        })
+        .attr("width", chartInnerWidth / csvData.length - 1)
+        .attr("x", function(d, i){
+            return i * (chartInnerWidth / csvData.length) + leftPadding;
+        })
+        .attr("height", function(d, i){
+            return 463 - yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+        .style("fill", function(d){
+            return colorScale(d[expressed]);
+        });
+
+    //create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 40)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Number of Variable " + expressed + " in each region");
+
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+
+    //annotate bars with attribute value text
+
+
+var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function(a, b){
+            return b[expressed]-a[expressed]
+        })
+        .attr("class", function(d){
+            return "numbers " + d.NAME;
+        })
+        .attr("x", function(d, i){
+            return i * (chartInnerWidth / csvData.length) + leftPadding;
+        })
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+        .text(function(d){
+            return d[expressed];
+        });
+
+    };
+})();
