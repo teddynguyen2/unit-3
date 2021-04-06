bf9b0037ce7eee5261d3088cf184785ee2518e33
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-04-05 23:36:46 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function setMap(){
 
     //create Albers equal area conic projection centered on Spain
     var projection = d3.geoAlbers()
-        .center([0, 30])
+        .center([0, 35])
         .rotate([-2, 0])
         .parallels([43, 62])
         .scale(2500)
