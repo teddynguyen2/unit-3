43bd03a7d9e1feaa5c54a646a5b044e44337961d
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-04-13 02:58:34 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,6 +26,29 @@
     //create a scale to size bars proportionally to frame and for axis
     var yScale = d3.scaleLinear().range([463, 0]).domain([0, 100]);
   
+    
+    var quantize = d3.scaleQuantize()
+      .domain([ 0, 0.15 ])
+      .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
+    var svg = d3.select("svg");
+
+    svg.append("g")
+      .attr("class", "legendQuant")
+      .attr("transform", "translate(20,20)");
+
+    var legend = d3.legendColor()
+      .labelFormat(d3.format(".2f"))
+      .useClass(true)
+      .title("A really really really really 
+        really long title")
+      .titleWidth(100)
+      .scale(quantize);
+
+    svg.select(".legendQuant")
+      .call(legend);
+    
+    
     //begin script when window loads
     window.onload = setMap();
 
