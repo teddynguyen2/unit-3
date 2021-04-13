be52bde43e49016c9809a3bf446527e01e89e715
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-04-12 21:57:26 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -288,6 +288,8 @@ function createDropdown(){
     var dropdown = d3.select("body")
         .append("select")
         .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
         });
 
     //add initial option
