c4c093559777a14364b1e745da330dcaa697ce9c
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-04-13 20:30:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -208,7 +208,7 @@
 
         //create a second svg element to hold the bar chart
         var chart = d3
-            .select("#body")
+            .select("body")
             .append("svg")
             .attr("width", chartWidth)
             .attr("height", chartHeight)
